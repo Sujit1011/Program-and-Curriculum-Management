fff3973749cb9d924a5de50680c34b4570f1cbb0
Sujit Soren <sujitsoren.gms@gmail.com>
2021-03-13 23:51:38 +0530

    Updated files

---
+++ PCM_FusionIIIT/program_and_curriculum_management/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from program_and_curriculum_management import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('programme',views.programme, name='program_and_curriculum_management'),
+]
